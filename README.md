f7e815f95e3ee848aca6451022d694ed4376dc80
Rithika Ganesan <rganesan@cern.ch>
2026-09-18 03:42:50 +0200

    updated batch_jobs

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ To run single jobs from the command line, add the following line to your bash_pr
 ```
 alias crabdriver="python3 /afs/cern.ch/user/**j**/**janedoe**/private/crab-helper/driver.py"
 ```
+
+To edit and multiple jobs, add to bash_profile:
+```
+crabbatcher="/afs/cern.ch/user/r/rganesan/private/crab-helper/batch_jobs.py"
+``` 
+and run:
+```
+code $crabbatcher       # to edit. use whatever text editor to open the file and change the params
+python3 $crabbatcher    # run 
+```
--- batch_jobs.py
+++ batch_jobs.py
@@ -1,8 +1,17 @@
 import subprocess 
 
-modules = ['IR', 'VMR', 'TB', 'MP', 'PC', 'TP', 'TPD', 'DR', 'ALL', 'NONE']
+# all options 
+# modules = ['IR', 'VMR', 'TB', 'MP', 'PC', 'TP', 'TPD', 'DR', 'ALL', 'NONE']
+# samples = ['DispSUSY', 'Higgs900', 'Higgs1500']
+# algos = ['HYBRID', 'HYBRID_DISPLACED', 'HYBRID_NEWKF', 'HYBRID_SIM', 'HYBRID_SIM_DISPLACED']
+
+# chosen options 
+
+build = True
+test = False
+modules = ['ALL', 'NONE']
 samples = ['DispSUSY', 'Higgs900', 'Higgs1500']
-algos = ['HYBRID', 'HYBRID_DISPLACED', 'HYBRID_NEWKF', 'HYBRID_SIM', 'HYBRID_SIM_DISPLACED']
+algos = ['HYBRID'] 
 
 n_events_by_sample = {
     'DispSUSY': 10000, 'Higgs900': 50000, 'Higgs1500': 50000
@@ -11,3 +20,36 @@ n_events_by_sample = {
 num_threads_by_sample = {
     'DispSUSY': 1, 'Higgs900': 1, 'Higgs1500': 1
 }
+
+script_path = "/afs/cern.ch/user/r/rganesan/private/crab-helper/driver.py" 
+
+# loop 
+# does not rebuild per module
+
+for module in modules:
+    for si, sample in enumerate(samples):
+        n_events, num_threads = n_events_by_sample[sample], num_threads_by_sample[sample]
+
+        for ai, algo in enumerate(algos):
+            new_build = (si == 0) and (ai == 0) and (build == True)
+
+            if algo == "HYBRID" and sample == "Higgs900" and module == "ALL": 
+                continue
+
+            command = [
+                "python3", script_path,
+                "--module", module,
+                "--l1tf_algorithm", algo,
+                "--n_events", str(n_events),
+                "--data_name", sample,
+                "--num_threads", str(num_threads),
+            ]
+
+            if not new_build:
+                command.append("--nobuild")
+
+            if test == True:
+                command.append("--test")
+
+            print(f"Running: {' '.join(command)}")
+            subprocess.run(command, check=True)
